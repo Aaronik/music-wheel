98b5a20998864bf900d17ac4567c5c6620af9c45
Aaron Sullivan <aasullivan1618@gmail.com>
2022-11-07 00:51:12 -0800

    Got modals for legend/instructions
    Also the beginnings of note playing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
+import * as Tone from 'tone'
 import { useState } from 'react'
 import './App.css'
+
+const NOTES = [
+  'A4', 'Bb4', 'B4', 'C5', 'Db5', 'D5', 'Eb5', 'E5', 'F5', 'F#5', 'G5', 'Ab5',
+  'A5', 'Bb5', 'B5', 'C6', 'Db6', 'D6', 'Eb6', 'E6', 'F6', 'F#6', 'G6', 'Ab6',
+  'A6'
+]
+
+const synth = new Tone.Synth().toDestination()
 
 function App() {
 
   const [noteRotation, setNoteRotation] = useState(0)
   const [lineRotation, setLineRotation] = useState(0)
+  const [isLegendVisible, setLegendVisible] = useState(false)
+  const [isInstructionsVisible, setInstructionsVisible] = useState(false)
 
   const noteRotationStyle = { transform: `rotate(${noteRotation}deg)` }
   const lineRotationStyle = { transform: `rotate(${lineRotation}deg)` }
-
-  console.log(noteRotationStyle, lineRotationStyle)
 
   const incrementNoteRotation = () => setNoteRotation(noteRotation + 30)
   const decrementNoteRotation = () => setNoteRotation(noteRotation - 30)
   const incrementLineRotation = () => setLineRotation(lineRotation + 30)
   const decrementLineRotation = () => setLineRotation(lineRotation - 30)
 
+  // const play = () => {
+  //   const notes =
+  //   notes.forEach((note, index) => {
+  //     synth.triggerAttackRelease(note, "8n", Tone.now() + (index / 3))
+  //   })
+  // }
+
+  let legendClassName = 'modal'
+  if (!isLegendVisible) legendClassName += ' hidden'
+  let instructionsClassName = 'modal'
+  if (!isInstructionsVisible) instructionsClassName += ' hidden'
+
+
   return (
     <div className="App">
-      <div className='hidden'><img src='legend.png' /></div>
+      <div id='legend' onClick={() => setLegendVisible(false)} className={legendClassName}><img src='legend.png' /></div>
+      <div id='instructions' onClick={() => setInstructionsVisible(false)} className={instructionsClassName}><img src='instructions.png' /></div>
+      <div id='help-buttons'>
+        <h2 onClick={() => setLegendVisible(!isLegendVisible)}>Show Legend</h2>
+        <h2 onClick={() => setInstructionsVisible(!isInstructionsVisible)}>Show Instructions</h2>
+      </div>
       <div id='note-wheel-buttons' className='rotate-buttons'>
         <h2>Note Rotation</h2>
         <div>
@@ -38,7 +65,6 @@ function App() {
         <img style={noteRotationStyle} id='note-wheel' className='overlapped rotatable' src='note-wheel.png' />
         <img style={lineRotationStyle} id='line-wheel' className='overlapped rotatable' src='line-wheel.png' />
       </div>
-      <div className='hidden'><img src='instructions.png' /></div>
     </div>
   )
 }
