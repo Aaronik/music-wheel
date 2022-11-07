167256c4d2915c3ddf8fecb2a378599c94f11411
Aaron Sullivan <aasullivan1618@gmail.com>
2022-11-07 00:00:23 -0800

    Initial commit!
    Got some rotating wheels

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+
+  const [noteRotation, setNoteRotation] = useState(0)
+  const [lineRotation, setLineRotation] = useState(0)
+
+  const noteRotationStyle = { transform: `rotate(${noteRotation}deg)` }
+  const lineRotationStyle = { transform: `rotate(${lineRotation}deg)` }
+
+  console.log(noteRotationStyle, lineRotationStyle)
+
+  const incrementNoteRotation = () => setNoteRotation(noteRotation + 30)
+  const decrementNoteRotation = () => setNoteRotation(noteRotation - 30)
+  const incrementLineRotation = () => setLineRotation(lineRotation + 30)
+  const decrementLineRotation = () => setLineRotation(lineRotation - 30)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='hidden'><img src='legend.png' /></div>
+      <div id='note-wheel-buttons' className='rotate-buttons'>
+        <h2>Note Rotation</h2>
+        <div>
+          <div onClick={incrementNoteRotation}>⇧</div>
+          <div onClick={decrementNoteRotation}>⇩</div>
+        </div>
+      </div>
+      <div id='line-wheel-buttons' className='rotate-buttons'>
+        <h2>Color Rotation</h2>
+        <div>
+          <div onClick={incrementLineRotation}>⇧</div>
+          <div onClick={decrementLineRotation}>⇩</div>
+        </div>
+      </div>
+      <div id='wheel-container'>
+        <img id='quality-wheel' className='overlapped' src='quality-wheel.png' />
+        <img style={noteRotationStyle} id='note-wheel' className='overlapped rotatable' src='note-wheel.png' />
+        <img style={lineRotationStyle} id='line-wheel' className='overlapped rotatable' src='line-wheel.png' />
+      </div>
+      <div className='hidden'><img src='instructions.png' /></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
