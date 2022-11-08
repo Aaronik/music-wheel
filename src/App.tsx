8d36b9a4c78e8d49849d3b0d5403a37ae0ec01fe
Aaron Sullivan <aasullivan1618@gmail.com>
2022-11-07 16:02:42 -0800

    About done but for one bug:
    We're running out of notes in our scale quite quickly and having
    undefinedes be in there

--- src/App.tsx
+++ src/App.tsx
@@ -3,54 +3,96 @@ import { useState } from 'react'
 import './App.css'
 
 const NOTES = [
-  'A4', 'Bb4', 'B4', 'C5', 'Db5', 'D5', 'Eb5', 'E5', 'F5', 'F#5', 'G5', 'Ab5',
-  'A5', 'Bb5', 'B5', 'C6', 'Db6', 'D6', 'Eb6', 'E6', 'F6', 'F#6', 'G6', 'Ab6',
-  'A6'
+  'C4', 'Db4', 'D4', 'Eb4', 'E4', 'F4', 'F#4', 'G4', 'Ab4', 'A4', 'Bb4', 'B4',
+  'C5', 'Db5', 'D5', 'Eb5', 'E5', 'F5', 'F#5', 'G5', 'Ab5', 'A5', 'Bb5', 'B5',
+  'C6'
 ]
 
-const synth = new Tone.Synth().toDestination()
-
 // TODO
-// * Legend always visible on top
-// * Play row
 // * modal background
-// * Rotation degrees modulo 360
+// * Rotating notes down leaves undefinedes in scale
 
 function App() {
 
-  const [noteRotation, setNoteRotation] = useState(0)
-  const [lineRotation, setLineRotation] = useState(0)
-  const [isLegendVisible, setLegendVisible] = useState(false)
+  const [noteIndex, setNoteIndex] = useState(0)
+  const [modeIndex, setModeIndex] = useState(0)
   const [isInstructionsVisible, setInstructionsVisible] = useState(false)
 
-  const noteRotationStyle = { transform: `rotate(${noteRotation}deg)` }
-  const lineRotationStyle = { transform: `rotate(${lineRotation}deg)` }
+  const incrementNoteRotation = () => setNoteIndex(noteIndex + 1)
+  const decrementNoteRotation = () => setNoteIndex(noteIndex - 1)
+  const incrementModeRotation = () => setModeIndex(modeIndex + 1)
+  const decrementModeRotation = () => setModeIndex(modeIndex - 1)
 
-  const incrementNoteRotation = () => setNoteRotation(noteRotation + 30)
-  const decrementNoteRotation = () => setNoteRotation(noteRotation - 30)
-  const incrementLineRotation = () => setLineRotation(lineRotation + 30)
-  const decrementLineRotation = () => setLineRotation(lineRotation - 30)
+  // Basically whole step/half step
+  const scaleSteps = [2, 2, 1, 2, 2, 2, 1, 2] // Starting on Ionian
+  const chromaticScale = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
-  // const play = () => {
-  //   const notes =
-  //   notes.forEach((note, index) => {
-  //     synth.triggerAttackRelease(note, "8n", Tone.now() + (index / 3))
-  //   })
-  // }
+  // Translate to the correct scaleSteps for the mode we're in
+  for (let i = 0; i < modeIndex; i++) { // TODO Maybe change modeIndex to modeRotationIndex
+    scaleSteps.push(scaleSteps.shift() as number)
+  }
 
-  // let legendClassName = 'modal'
-  // if (!isLegendVisible) legendClassName += ' hidden'
+  // Generate the scale degrees we're concerned with by applying scaleSteps to chromaticScale
+  const scaleDegrees: number[] = []
+  let currentPosition = 0
+  for (let step of scaleSteps) {
+    scaleDegrees.push(chromaticScale[currentPosition])
+    currentPosition += step
+  }
+
+  // See, if we just did mode index like note index, the modes would rotate by their 30 degrees
+  // and look fine, but 5 times, the root note _would not be on a note_, and that's semantically
+  // undefined. So by having this secondary index, we can skip indices that are undesirable.
+  const ionianScaleDegrees = [0, 2, 4, 5, 7, 9, 11] // B/c this is our starting place, all rotations are relative to the ionian mode
+  const modeRotationIndex = ionianScaleDegrees[modeIndex % ionianScaleDegrees.length]
+
+  const noteRotationStyle = { transform: `rotate(${(0 - noteIndex) * 30}deg)` }
+  const modeRotationStyle = { transform: `rotate(${(0 - modeRotationIndex) * 30}deg)` }
+
+  // const rootNote = NOTES[noteIndex % 12]
+  const notes: string[] = []
+  for (const i of scaleDegrees) {
+    notes.push(NOTES[i + noteIndex])
+  }
+  console.log('scale: ', notes)
+
+  const playNotes = async (toPlay: string[], shouldPlayTogether = false) => {
+    await Tone.start()
+
+    const synth = new Tone.PolySynth(Tone.Synth).toDestination()
+
+    toPlay.forEach((note, index) => {
+      synth.triggerAttackRelease(note, "8n", Tone.now() + (index / 3))
+    })
+
+    if (shouldPlayTogether) {
+      synth.triggerAttackRelease(toPlay, "4n", Tone.now() + ((toPlay.length + 0.5) / 3))
+    }
+  }
+
+  const playScale = () => {
+    playNotes(notes)
+  }
+
+  const playTriad = () => {
+    const toPlay = [notes[0], notes[2], notes[4]]
+    playNotes(toPlay, true)
+  }
+
+  const playSeventh = () => {
+    const toPlay = [notes[0], notes[2], notes[4], notes[6]]
+    playNotes(toPlay, true)
+  }
+
   let instructionsClassName = 'modal'
   if (!isInstructionsVisible) instructionsClassName += ' hidden'
 
 
   return (
     <div className="App">
-      { /* <div id='legend' onClick={() => setLegendVisible(false)} className={legendClassName}><img src='legend.png' /></div> */ }
       <div id='legend-container'><img src='legend.png' width={'80%'} /></div>
       <div id='instructions' onClick={() => setInstructionsVisible(false)} className={instructionsClassName}><img src='instructions.png' /></div>
       <div id='help-buttons'>
-        {/* <h2 onClick={() => setLegendVisible(!isLegendVisible)}>Show Legend</h2> */ }
         <h2 onClick={() => setInstructionsVisible(!isInstructionsVisible)}>?</h2>
       </div>
       <div id='wheel-buttons-container'>
@@ -61,23 +103,23 @@ function App() {
             <button onClick={decrementNoteRotation}>⇩</button>
           </div>
         </div>
-        <div id='line-wheel-buttons' className='rotate-buttons'>
+        <div id='mode-wheel-buttons' className='rotate-buttons'>
           <h2>Select Mode</h2>
           <div>
-            <button onClick={incrementLineRotation}>⇧</button>
-            <button onClick={decrementLineRotation}>⇩</button>
+            <button onClick={incrementModeRotation}>⇧</button>
+            <button onClick={decrementModeRotation}>⇩</button>
           </div>
         </div>
       </div>
       <div id='play-buttons-container'>
-        <button onClick={() => {}}>🎧 Scale</button>
-        <button onClick={() => {}}>🎧 Triad</button>
-        <button onClick={() => {}}>🎧 Seventh</button>
+        <button onClick={playScale}>🎧 Scale</button>
+        <button onClick={playTriad}>🎧 Triad</button>
+        <button onClick={playSeventh}>🎧 Seventh</button>
       </div>
       <div id='wheel-container'>
         <img id='quality-wheel' className='wheel' src='quality-wheel.png' />
         <img style={noteRotationStyle} id='note-wheel' className='wheel rotatable' src='note-wheel.png' />
-        <img style={lineRotationStyle} id='line-wheel' className='wheel rotatable' src='line-wheel.png' />
+        <img style={modeRotationStyle} id='mode-wheel' className='wheel rotatable' src='mode-wheel.png' />
       </div>
     </div>
   )
