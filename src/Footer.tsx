c34e3f33655bf55e9d1e1b03999ffd424e9b9419
Aaron Sullivan <aasullivan1618@gmail.com>
2022-11-25 12:30:52 -0800

    Remove github button
    This is fine because there's still ultimately a link to it via
    aaronik.com

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -18,11 +18,11 @@ type FooterProps = {
   onHelpClick: () => void
 }
 
+      // <ButtonAndLink url="https://github.com/aaronik/music-wheel"><img id="github" src="github.png" /></ButtonAndLink>
 export default function Footer({ onHelpClick }: FooterProps) {
   return (
     <div id="footer-container">
       <button onClick={onHelpClick}>?</button>
-      <ButtonAndLink url="https://github.com/aaronik/music-wheel"><img id="github" src="github.png" /></ButtonAndLink>
       <ButtonAndLink url="https://www.paypal.com/donate/?hosted_button_id=22EGXEL2CWHCN">Donate</ButtonAndLink>
     </div>
   )
