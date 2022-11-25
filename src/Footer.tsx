f95e9732c11e090cb06fe4e63f4a7a023e1cbfb9
Aaron Sullivan <aasullivan1618@gmail.com>
2022-11-25 12:26:58 -0800

    Support us -> Donate

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -23,7 +23,7 @@ export default function Footer({ onHelpClick }: FooterProps) {
     <div id="footer-container">
       <button onClick={onHelpClick}>?</button>
       <ButtonAndLink url="https://github.com/aaronik/music-wheel"><img id="github" src="github.png" /></ButtonAndLink>
-      <ButtonAndLink url="https://www.paypal.com/donate/?hosted_button_id=22EGXEL2CWHCN">Support us ♡</ButtonAndLink>
+      <ButtonAndLink url="https://www.paypal.com/donate/?hosted_button_id=22EGXEL2CWHCN">Donate</ButtonAndLink>
     </div>
   )
 }
