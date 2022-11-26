33ed102684aa521ebe1491963a6a1c27c8ae8ee5
Aaron Sullivan <aasullivan1618@gmail.com>
2022-11-26 14:37:22 -0800

    We need more notes to go along with all the new octaves

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,8 @@ export const NOTES = [
   'C3', 'Db3', 'D3', 'Eb3', 'E3', 'F3', 'F#3', 'G3', 'Ab3', 'A3', 'Bb3', 'B3',
   'C4', 'Db4', 'D4', 'Eb4', 'E4', 'F4', 'F#4', 'G4', 'Ab4', 'A4', 'Bb4', 'B4',
   'C5', 'Db5', 'D5', 'Eb5', 'E5', 'F5', 'F#5', 'G5', 'Ab5', 'A5', 'Bb5', 'B5',
-  'C6'
+  'C6', 'Db6', 'D6', 'Eb6', 'E6', 'F6', 'F#6', 'G6', 'Ab6', 'A6', 'Bb6', 'B6',
+  'C7'
 ]
 
 export const rootNoteByModeDisplayMap: { [root: string]: { [mode in Mode]: string } } = {
